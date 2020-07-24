3250c11372123bc729c786200dbe13888172dd33
dewacandra4 <45729819+dewacandra4@users.noreply.github.com>
2020-07-24 22:14:38 +0800

    Update UI

--- application/views/customer/profile.php
+++ application/views/customer/profile.php
@@ -1,35 +1,31 @@
 
         <!-- Begin Page Content -->
-            <div class="col-lg-10 mx-auto">
+            <div class="col-lg-7 mx-auto py-5">
             <?= $this->session->flashdata('message'); ?>
 
               <!-- Illustrations -->
              
   
-              <div class="card shadow mb-4" >
-                <div class="card-header py-3">
+              <div class="card shadow px-0 mb-5 mt-5" >
+                <div class="card-header py-4">
                 <h5 class="m-0 font-weight-bold text-dark"><?=$title;?></h5>
               </div>
-                            <div class="row no-gutters">
-                                <div class="col-md-4"></br>
-                                <div class="card-body">
-                                    <p class="card-text"><b>Username: </b><?=$user['username'];?></p></br>
-                                    <p class="card-text"><b>Name          :</b> <?=$user['name'];?></p></br>
-                                    <p class="card-text"><b>Email         : </b><?=$user['email'];?></p></br>
-                                    <p class="card-text"><b>Phone Number  : </b><?=$user['phone'];?></p></br>
-                                    <p class="card-text"><b>Address       : </b><?=$user['address'];?></p></br></br>
-                                    <p class="card-text"><small class="text-muted"><b>Member since </b> <?= date('d F yy', $user['date_created']);?></small></p>
-                                </div>
-                                </div>
-                                <div class="col-md-8"></br></br>
-                                &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp<img class="img-profile rounded-circle" src="<?=base_url('assets/dp/').$user['image'];?>" class="card-img" alt="..."></br></br>
-                                <p><strong> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp Profile Picture</strong></p>
-                                
-                     
-                   
-              
-              </div>
-              </div>
+                  <div class="row mx-auto py-5">
+                    <div class="col-md-7 ">
+                      <div class="card-body">
+                          <p class="card-text"><b>Username: </b><?=$user['username'];?></p></br>
+                          <p class="card-text"><b>Name          :</b> <?=$user['name'];?></p></br>
+                          <p class="card-text"><b>Email         : </b><?=$user['email'];?></p></br>
+                          <p class="card-text"><b>Phone Number  : </b><?=$user['phone'];?></p></br>
+                          <p class="card-text"><b>Address       : </b><?=$user['address'];?></p></br>
+                          <p class="card-text"><small class="text-muted"><b>Member since </b> <?= date('d F yy', $user['date_created']);?></small></p>
+                      </div>
+                    </div>
+                    <div class="col-md-5 mt-4">
+                      <img class="img-thumbnail rounded-circle text-center " src="<?=base_url('assets/dp/').$user['image'];?>" class="card-img" alt="..."></br></br>
+                      <p class="text-center" ><strong>Profile Picture</strong></p>
+                    </div>
+                </div>
               </div>
         </div>
         <!-- /.container-fluid -->
