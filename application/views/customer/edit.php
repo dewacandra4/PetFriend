d3f132c35e00275e23fd1c3c709d1e6703f102c9
Georama <geogeabulan32@gmail.com>
2020-08-06 20:24:55 +0800

    fix issue on 786 screen size

--- application/views/customer/edit.php
+++ application/views/customer/edit.php
@@ -34,13 +34,15 @@
                 </div>
                 <div class="row mt-2">
                 <label class="ml-3 mb-3" for="Username"><strong>Username : </strong><?=$user['username'];?></label></br>
-                <label class="ml-5 mb-3" for="Email"><strong>Email : </strong><?=$user['email'];?></label>
-                    <div class="col-md-12"><input type="text" class="form-control" id="name" name="name" placeholder="Name" value="<?=$user['name'];?>">
-                    <?= form_error('name','<small class="text-danger pl-3">','</small>'); ?>
-                    </div>
+                <label class="ml-3 mb-3" for="Email"><strong>Email : </strong><?=$user['email'];?></label>
                 </div>
                 <div class="row mt-3">
-                    <div class="col-md-12"><input type="text" class="form-control fix" id="phone" name="phone" value="<?=$user['phone'];?>" placeholder="Phone Number">
+                <div class="col-md-12"><input type="text" class="form-control" id="name" name="name" placeholder="Name" value="<?=$user['name'];?>">
+                    <?= form_error('name','<small class="text-danger pl-3">','</small>'); ?>
+                    </div>
+                    </div>
+                <div class="row mt-3">
+                    <div class="col-md-12 fix2"><input type="text" class="form-control fix" id="phone" name="phone" value="<?=$user['phone'];?>" placeholder="Phone Number">
                     <?= form_error('phone','<small class="text-danger pl-3">','</small>'); ?>
                     </div>
                 </div>
