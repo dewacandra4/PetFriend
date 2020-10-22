16b5244a3a1c43b9604180405181fac2d758e344
Georama <geogeabulan32@gmail.com>
2020-10-22 17:38:14 +0800

    update pet hotel order

--- application/views/home/pethotelo.php
+++ application/views/home/pethotelo.php
@@ -9,7 +9,7 @@
       <div class="row">
         <div class="col-xl-12 mx-auto">
         <img src="<?= base_url().'/assets/services/service_icon_1.png'?>" class="img-fluid">
-          <h1 class="text-white">Entrust Your Pets To PetFriend <br> For Us There is Only One Boss, Your Pet.
+          <h1 class="text-white">Entrust Your Pets To PetFriend <br> "For Us There is Only One Boss, Your Pet."
             </h1>
         </div>
       </div>
@@ -23,6 +23,7 @@
     <?php foreach ($services as $s) : ?>
     <p class="lead mb-0"><?= $s->description ?>
     <br>We provide various types of room types, with prices starting from RM <?= $s->price ?> / Night</p>
+    <br><a href="#order" class="add_cart btn btn-cart  rounded py-3">Book Now</a>
     </div>
   </section>
 
@@ -35,10 +36,10 @@
         <div class="col-lg-6 order-lg-1 my-auto showcase-text">
           <h2>Standar Room</h2>
           <p class="lead mb-0">
-          There are many facilities that your pet will get in the standard room,<br>
-            + wide playing field<br>
-            + 1 Toy<br>
-            + Breakfast and dinner<br>
+          There are many facilities that your pet will get in the standard room, such as : <br><br>
+            + Wide Playing Field<br>
+            + Breakfast and Dinner<br>
+            + Room with AC<br>
             + Free Snack<br>
             <br>
             Price : RM <?= $s->price ?> / Night (For Cat or Dog)
@@ -51,92 +52,120 @@
         <div class="col-lg-6 my-auto showcase-text">
           <h2>Deluxe Room</h2>
           <p class="lead mb-0">
-          We also offer a Deluxe Room, with more facilities such as,<br>
-            + wide playing field<br>
+          We also offer a Deluxe Room, with more facilities such as : <br><br>
+            + Wide Playing Field<br>
             + Swiming Pool (For dog)<br>
             + Sand Box (For cat)<br>
-            + 2 Toy<br>
-            + Room with AC<br>
-            + Breakfast and dinner (Organic Food)<br>
+            + Room with AC and Medium Size Bed<br>
+            + Breakfast and Dinner (Organic Food)<br>
             + Walk Session Every Morning<br>
             + Free Snack<br>
             <br>
-            Price : RM 40.00 / Night (For Cat or Dog)
+            Price : RM 15.00 / Night (For Cat or Dog)
           </p>
         </div>
       </div>
       <div class="row no-gutters">
         <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url(<?= base_url().'/assets/img/bg-showcase-3.jpg'?>);"></div>
         <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-          <h2>Easy to Use &amp; Customize</h2>
-          <p class="lead mb-0">Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!</p>
+          <h2>Royale Room</h2>
+          <p class="lead mb-0">
+          We also offer a Royale Room, with more facilities such as : <br><br>
+            + Individual Playing Field<br>
+            + Swiming Pool (For dog)<br>
+            + Sand Box (For cat)<br>
+            + Room with AC and Big Size Bed<br>
+            + Breakfast, Lunch and Dinner (Organic Food)<br>
+            + Walk Session Every Morning and Pet Salon<br>
+            + Free Snack<br>
+            <br>
+            Price : RM 25.00 / Night (For Cat or Dog)
+          </p>
         </div>
       </div>
     </div>
   </section>
 
   <!-- Keuntungan -->
-  <section class="features-icons bg-light text-center">
+  <section class="features-icons bg-white text-center" id="order" style="background-image: url(<?= base_url().'/assets/img/bg2.jpg'?>);">
+  <h2>Booking Form</h2> 
+  <?php if($s->is_available == 0) :?>
+                  <div class="alert alert-warning" role="alert"><h3>
+                  We are so sorry, there is no available room for now <i class="fa fa-frown-o" aria-hidden="true"></i></h3>
+                </div>
+    <?php else:?>
     <div class="container">
-      <div class="row">
-        <div class="col-lg-4">
-          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-            <div class="features-icons-icon d-flex">
-              <i class="icon-screen-desktop m-auto text-primary"></i>
-            </div>
-            <h3>Fully Responsive</h3>
-            <p class="lead mb-0">This theme will look great on any device, no matter the size!</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-            <div class="features-icons-icon d-flex">
-              <i class="icon-layers m-auto text-primary"></i>
-            </div>
-            <h3>Bootstrap 4 Ready</h3>
-            <p class="lead mb-0">Featuring the latest build of the new Bootstrap 4 framework!</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="features-icons-item mx-auto mb-0 mb-lg-3">
-            <div class="features-icons-icon d-flex">
-              <i class="icon-check m-auto text-primary"></i>
-            </div>
-            <h3>Easy to Use</h3>
-            <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+          <div class="card-body p-0">
+            <div class="row">
+              <div class="col-lg-7">
+                <div class="p-5">
+                <h4><i class="fa fa-suitcase" aria-hidden="true"></i>
+                Total Available Slot : <?= $s->resource ?></h4> 
+                <hr>
+                  <div class="text-center">
+                  </div>
+                  <br>
+                  <form method="post" action="<?= base_url('auth/login')?>" class="user">
 
-  <!-- Call to Action -->
-  <section class="call-to-action text-white text-center">
-    <div class="overlay"></div>
-    <div class="container">
-      <div class="row">
-        <div class="col-xl-9 mx-auto">
-          <h2 class="mb-4">Ready to get started? Sign up now!</h2>
-        </div>
-        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
-          <form>
-            <div class="form-row">
-              <div class="col-12 col-md-9 mb-2 mb-md-0">
-                <input type="email" class="form-control form-control-lg" placeholder="Enter your email...">
+                  <div class="row">
+
+                    <div class="col">
+                    <div class="form-group">
+                    <h5><i class="fa fa-calendar" aria-hidden="true"></i>
+                    Check-in Date: </h5>
+                    <input type="date" class="form-control " id="fromDate" name="check-in" placeholder="From Date" min="<?php echo date('Y-m-d'); ?>">
+                    </div>
+                    </div>
+
+                    <div class="col">
+                    <div class="form-group">
+                    <h5> <i class="fa fa-moon-o" aria-hidden="true"></i>
+                    How Many Days : </h5>
+                    <input type="number" name="days" id="days" class="form-control" value="" min="1" required>
+                    </div>
+                    </div>
+
+                    </div>
+
+                    <div class="form-group">
+                    <h5> <i class="fa fa-paw" aria-hidden="true"></i>
+                    Select Pet Kind :  </h5>
+                    <select class="form-control" name="petkind">
+                    <option>Dog</option>
+                    <option>Cat</option>
+                    </select>
+                    </div>
+
+                    <div class="form-group">
+                    <h5> <i class="fa fa-joomla" aria-hidden="true"></i>
+                    Select Room Type :  </h5>
+                    <select class="form-control" name="roomtype">
+                    <option>Royale</option>
+                    <option>Deluxe</option>
+                    <option>Standard</option>
+                    </select>
+                    </div>
+
+                    <br>
+                      <button type="submit" class="btn genric-btn danger-border circle btn-block">Book</button>
+                    <br>
+                  </form>
+                </div>
               </div>
-              <div class="col-12 col-md-3">
-                <button type="submit" class="btn btn-block btn-lg btn-primary">Sign up!</button>
-              </div>
+              <div class="col-lg-5 d-none d-lg-block"> <img class="rounded" src="<?= base_url()?>assets/img/Order.jpg" alt="beauty_dog" height="500px"></div>
             </div>
-          </form>
-        </div>
-      </div>
+          </div>
     </div>
   </section>
+  <?php endif; ?>
 </body>
 
 </html>
+
 <style>
+html {
+  scroll-behavior: smooth;
+}
 body {
   font-family: 'Lato', 'Helvetica Neue', Helvetica, Arial, sans-serif;
 }
@@ -233,29 +262,4 @@ header.masthead h1 {
   max-width: 12rem;
   box-shadow: 0px 5px 5px 0px #adb5bd;
 }
-
-.call-to-action {
-  position: relative;
-  background-color: #343a40;
-  background: url("../img/bg-masthead.jpg") no-repeat center center;
-  background-size: cover;
-  padding-top: 7rem;
-  padding-bottom: 7rem;
-}
-
-.call-to-action .overlay {
-  position: absolute;
-  background-color: #212529;
-  height: 100%;
-  width: 100%;
-  top: 0;
-  left: 0;
-  opacity: 0.3;
-}
-
-footer.footer {
-  padding-top: 4rem;
-  padding-bottom: 4rem;
-}
-
 </style>
