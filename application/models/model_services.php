4ee5d574f85cd9c37bd7f5b5f47fe694344875a0
dewacandra4 <45729819+dewacandra4@users.noreply.github.com>
2020-11-13 13:33:07 +0800

    fix some UI and Control

--- application/models/model_services.php
+++ application/models/model_services.php
@@ -4,22 +4,15 @@ class Model_services extends CI_Model{
     public function show_data(){
         return $this->db->get('services');
     }
-    // public function add_services($data,$table){
-    //     $this->db->insert($table,$data);
-    // }
-    public function edit_services($where,$table){
-        return $this->db->get_where($table,$where);
-    }
     public function update_data($where,$data,$table)
     {
         $this->db->where($where);
         $this->db->update($table,$data);
     }
-    // public function delete_data($where,$table)
-    // {
-    //     $this->db->where($where);
-    //     $this->db->delete($table);
-    // }
+    public function edit_services($where,$table){
+        return $this->db->get_where($table,$where);
+    }
+
     public function detail_service($id)
     {
         $result = $this->db->where('id',$id)->get('services');
