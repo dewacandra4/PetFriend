e026fa351526ac2d1eb10cd1d9bac3548c8332ab
Georama <geogeabulan32@gmail.com>
2020-12-03 13:05:32 +0800

    up

--- application/views/home/detail_product.php
+++ application/views/home/detail_product.php
@@ -329,11 +329,19 @@
                     <label for="name">Full Name</label>
                     <input readonly class="form-control" id="name" value="<?=$customer['name'];?>">
                 </div>
+                <div class="rating">
+                <span><input type="radio" name="rating" id="str5" value="5"><label for="str5"></label></span>
+                <span><input type="radio" name="rating" id="str4" value="4"><label for="str4"></label></span>
+                <span><input type="radio" name="rating" id="str3" value="3"><label for="str3"></label></span>
+                <span><input type="radio" name="rating" id="str2" value="2"><label for="str2"></label></span>
+                <span><input type="radio" name="rating" id="str1" value="1"><label for="str1"></label></span>
+                </div>
                 <div class="form-group">
                     <label for="sub">Subject</label>
-                    <input type="text" class="form-control" id="sub" placeholder="Few words about the product" required>
+                    <input type="text" class="form-control" id="sub" placeholder="Few word about the product" required>
                 </div>
-                    <textarea id="area" placeholder="Write your review here!" maxlength="200" class="pb-cmnt-textarea"></textarea>
+                <label for="area">Review Content</label>
+                    <textarea id="area" placeholder="Write your review here!  (Optional)" maxlength="200" class="pb-cmnt-textarea"></textarea>
                     <div class="word-counter mr-3">0/200</div>
                         <input type="hidden" name="user_id" value="<?=$customer['id'];?>">
                         <button class="btn btn-cart p-3 mt-3" type="button">Submit <i class="fas fa-pen-square"></i></button>
@@ -532,10 +540,56 @@ right:0;
   }
 }
 
+.rating {
+    float:rigth;
+    width:300px;
+}
+.rating span { float:right; position:relative; }
+.rating span input {
+    position:absolute;
+    top:0px;
+    left:0px;
+    opacity:0;
+}
+.rating span label {
+    display:inline-block;
+    width:30px;
+    height:30px;
+    text-align:center;
+    color:#FFF;
+    background:#ccc;
+    font-size:30px;
+    margin-right:2px;
+    line-height:30px;
+    border-radius:50%;
+    -webkit-border-radius:50%;
+}
+.rating span:hover ~ span label,
+.rating span:hover label,
+.rating span.checked label,
+.rating span.checked ~ span label {
+    background:#F90;
+    color:#FFF;
+}
 </style>
+
 <script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
 <script>
 $('#area').keyup(function(){
     $('.word-counter').text($.trim(this.value.length)+'/200');
 })
+</script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js">
+</script>
+<script>
+    $(document).ready(function(){
+    // Check Radio-box
+    $(".rating input:radio").attr("checked", false);
+
+    $('.rating input').click(function () {
+        $(".rating span").removeClass('checked');
+        $(this).parent().addClass('checked');
+    }); 
+});
 </script>
